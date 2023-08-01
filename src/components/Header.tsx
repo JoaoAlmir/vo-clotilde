00c6280715c5f21d35c1a050bbee06cfe089c2dd
JoaoAlmir <junivion@gmail.com>
2023-07-31 22:12:41 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+function Header() {
+
+    return (
+        <>
+            <header className="d-flex justify-content-center">
+                <h5 className="text-danger p-5">AVES</h5>
+                <h5 className="text-danger p-5">BEBIDAS</h5>
+                <h5 className="text-danger p-5">BOLOS & TORTAS</h5>
+                <h5 className="text-danger p-5">CARNES VERMELHAS</h5>
+                <img  src="src/assets/vovo.png" alt="icon" />
+                <h5 className="text-danger p-5">DOCES E SOBEMESAS</h5>
+                <h5 className="text-danger p-5">MASSAS</h5>
+                <h5 className="text-danger p-5">PEIXES E FRUTOS DO MAR</h5>
+
+            </header>
+
+
+        </>
+    )
+}
+
+export default Header
