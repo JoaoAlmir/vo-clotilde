1980d9a5e76fbf2de56a4067107d9db44f70b32e
JoaoAlmir <junivion@gmail.com>
2023-08-02 18:50:57 -0300

    add search

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -3,14 +3,14 @@ function Header() {
     return (
         <>
             <header className="d-flex justify-content-center">
-                <h5 className="text-danger p-5">AVES</h5>
-                <h5 className="text-danger p-5">BEBIDAS</h5>
-                <h5 className="text-danger p-5">BOLOS & TORTAS</h5>
-                <h5 className="text-danger p-5">CARNES VERMELHAS</h5>
-                <img width="150px" src="src/assets/vovo.png" alt="icon" />
-                <h5 className="text-danger p-5">DOCES & SOBEMESAS</h5>
-                <h5 className="text-danger p-5">MASSAS</h5>
-                <h5 className="text-danger p-5">PEIXES & FRUTOS DO MAR</h5>
+                <h5 className="text-danger p-5 border-bottom border-danger-subtle rounded-5">AVES</h5>
+                <h5 className="text-danger p-5 border-bottom border-danger-subtle rounded-5">BEBIDAS</h5>
+                <h5 className="text-danger p-5 border-bottom border-danger-subtle rounded-5">BOLOS & TORTAS</h5>
+                <h5 className="text-danger p-5 border-bottom border-danger-subtle rounded-5">CARNES VERMELHAS</h5>
+                <img width="120px" height="120px" src="src/assets/headerIcon.png" alt="icon" />
+                <h5 className="text-danger p-5 border-bottom border-danger-subtle rounded-5">DOCES & SOBEMESAS</h5>
+                <h5 className="text-danger p-5 border-bottom border-danger-subtle rounded-5">MASSAS</h5>
+                <h5 className="text-danger p-5 border-bottom border-danger-subtle rounded-5">PEIXES & FRUTOS DO MAR</h5>
 
             </header>
 
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -4,7 +4,27 @@ function Home() {
 
     return (
         <>
-            <Header/>
+            <Header />
+
+            <div className="mb-3" style={{ backgroundImage: `url(src/assets/searchBackground.png)`, height: '500px', backgroundSize: 'cover' }}>
+                <div className="input-group top-50 w-50 mx-auto">
+                    <input type="text" className="form-control" placeholder="Pesquisar Receita" aria-label="Pesquisar Receita" aria-describedby="button-addon2" />
+                    <button className="btn btn-outline-light d-inline" type="button" id="search">Pesquisar</button>
+                </div>
+            </div>
+
+            <div className="d-flex justify-content-center">
+                <div className="border border-danger-subtle" style={{width:"380px",height:"780px"}}>
+                    <img width="380px" height="250px" src="https://guiadacozinha.com.br/wp-content/uploads/2020/10/donuts-simples-350x230.jpg" alt="icon" />
+                    <h3 className="p-5 text-danger">Donuts tradicional: aprenda a fazer versão sem cobertura   </h3>
+                    
+                    <span className="d-flex">
+                        <i className="bi bi-alarm"></i>
+                    </span>
+
+                </div>
+                <div></div>
+            </div>
 
         </>
     )
