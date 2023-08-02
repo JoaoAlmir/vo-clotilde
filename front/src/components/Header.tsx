ebd040ed2f1e3b70a10f337f6b231e2102305026
JoaoAlmir <junivion@gmail.com>
2023-08-02 16:53:12 -0300

    add Header

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -7,10 +7,10 @@ function Header() {
                 <h5 className="text-danger p-5">BEBIDAS</h5>
                 <h5 className="text-danger p-5">BOLOS & TORTAS</h5>
                 <h5 className="text-danger p-5">CARNES VERMELHAS</h5>
-                <img  src="src/assets/vovo.png" alt="icon" />
-                <h5 className="text-danger p-5">DOCES E SOBEMESAS</h5>
+                <img width="150px" src="src/assets/vovo.png" alt="icon" />
+                <h5 className="text-danger p-5">DOCES & SOBEMESAS</h5>
                 <h5 className="text-danger p-5">MASSAS</h5>
-                <h5 className="text-danger p-5">PEIXES E FRUTOS DO MAR</h5>
+                <h5 className="text-danger p-5">PEIXES & FRUTOS DO MAR</h5>
 
             </header>
 
