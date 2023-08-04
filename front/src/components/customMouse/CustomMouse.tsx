d9d581130cb41731e42191a6b98b82fd4e7b8fd3
JoaoAlmir <junivion@gmail.com>
2023-08-04 20:02:53 -0300

    remove mouse log

--- front/src/components/customMouse/CustomMouse.tsx
+++ front/src/components/customMouse/CustomMouse.tsx
@@ -8,9 +8,7 @@ export function CustomMouse() {
     const cursorRef:any = React.useRef(null)
 
     function renderMouse(event:any) {
-        const { clientX, clientY } = event;
-        console.log(event);
-        
+        const { clientX, clientY } = event;        
         const mouseX = clientX - 6
         const mouseY = clientY - 2
         cursorRef.current.style.transform = "translate3d(" + mouseX + "px, " + mouseY + "px,0)"
