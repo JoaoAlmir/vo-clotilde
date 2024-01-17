d826d759047c23a3183eef661a5cd7f66171375c
JoaoAlmir <joaoalmir@alu.ufc.br>
2024-01-17 19:34:00 -0300

    add rotas categorias

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,25 +1,33 @@
 import { NavLink } from "react-router-dom"
 
 function Header() {
-    
-
     return (
         <>
             <header className="d-flex justify-content-evenly position-fixed d-block">
-                
-                <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">BEBIDAS</h5>
-                <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">BOLOS & TORTAS</h5>
-                <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">CARNES</h5>
-                <NavLink end to="/"><img width="80px" height="80px" src="src/assets/headerIcon.png" alt="icon" /></NavLink>
-                <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">DOCES & SOBEMESAS</h5>
-                <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">MASSAS</h5>
-                <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">PEIXES & FRUTOS DO MAR</h5>
-
+                <NavLink end to="/">
+                    <img width="80px" height="80px" src="src/assets/headerIcon.png" alt="icon" />
+                </NavLink>
+                <NavLink end to="/categoria/bebidas">
+                    <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">BEBIDAS</h5>
+                </NavLink>
+                <NavLink end to="/categoria/bolosetortas">
+                    <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">BOLOS &amp; TORTAS</h5>
+                </NavLink>
+                <NavLink end to="/categoria/carnes">
+                    <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">CARNES</h5>
+                </NavLink>
+                <NavLink end to="/categoria/doces">
+                    <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">DOCES &amp; SOBREMESAS</h5>
+                </NavLink>
+                <NavLink end to="/categoria/massas">
+                    <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">MASSAS</h5>
+                </NavLink>
+                <NavLink end to="/categoria/peixes">
+                    <h5 className="p-4 text-danger rounded-bottom border-danger border border-bottom-0">PEIXES &amp; FRUTOS DO MAR</h5>
+                </NavLink>
             </header>
-
-
         </>
-    )
+    );
 }
 
 export default Header
