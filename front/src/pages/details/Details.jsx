30e90dbfedafabec088b78f5c2d9007cb36825a4
JoaoAlmir <joaoalmir@alu.ufc.br>
2024-06-20 06:49:15 -0300

    fix details text & add header icon animation

--- front/src/pages/details/Details.jsx
+++ front/src/pages/details/Details.jsx
@@ -15,12 +15,24 @@ function Details() {
     useEffect(() => {
         fetchItem(id).then((res) => {
             console.log(res.data)
+
+            res.data.ingredientes = res.data.ingredientes.split(',').map((ingrediente) => {
+
+                return <li>{ingrediente}</li>
+
+            }
+            )
+
+            res.data.preparo = res.data.preparo.split('.').map((preparo) => {
+                if (preparo !== "") {
+                    return <li>{preparo}</li>
+                }
+            }
+            )
+
             setItem(res.data)
-            let ig = res.data.ingredientes.split(",")
-            
-            let ingredientes = ig.map((item, index) => {
-                return <li key={index}>{item}</li>
-            })
+
+
             console.log(ingredientes)
 
         }
@@ -28,6 +40,7 @@ function Details() {
             console.log(err)
         })
     }, [])
+
 
 
 
@@ -58,48 +71,16 @@ function Details() {
 
             <div className="my-5 w-50 mx-auto fs-5">
                 <h2 className="text-danger">Ingredientes</h2>
-                <ul>
-                    {item.ingrediente}
-                </ul>
-            </div>
 
-            <div className="w-50 mx-auto">
+                {item.ingredientes}
 
-                <h2 className="text-danger">Ingredientes</h2>
-
-                <ul className="fs-5">
-                    <li>3 e 1/2 xícaras (chá) de farinha de trigo</li>
-                    <li>1 envelope de fermento biológico seco granulado</li>
-                    <li>1 colher (chá) de sal</li>
-                    <li>1/2 xícara (chá) de água morna</li>
-                    <li>1/2 xícara (chá) de leite morno</li>
-                    <li>1/4 de xícara (chá) de margarina</li>
-                    <li>1 ovo</li>
-                    <li>1/4 de xícara (chá) de açúcar</li>
-                    <li>Farinha de trigo para enfarinhar</li>
-                    <li>Óleo para fritar</li>
-                    <li>1/2 xícara (chá) de açúcar</li>
-                    <li>2 colheres (chá) de canela em pó</li>
-                </ul>
             </div>
 
 
-            <div className="w-50 mx-auto">
-
-                <h2 className="text-danger">Modo de preparo:</h2>
-
-                <ol className="fs-5 mb-5">
-                    <li>Na tigela grande da batedeira adicione 1 xícara (chá) de farinha, o açúcar, o fermento e o sal.</li>
-                    <li>Logo depois adicione gradualmente a água, o leite e a margarina misturados, batendo por 2 minutos em potência média limpando a tigela com uma espátula ocasionalmente.</li>
-                    <li>Adicione o ovo e mais 1 xícara (chá) de farinha e bata por mais 2 minutos.</li>
-                    <li>Sem bater, acrescente o restante da farinha misture, transfira para uma superfície lisa enfarinhada sove até obter uma massa elástica e macia.</li>
-                    <li>Cubra e deixe descansar 20 minutos.</li>
-                    <li>Com um rolo abra a massa com 2cm de espessura e corte usando um copo ou cortador redondo com aproximadamente 5cm de diâmetro e com um objeto menor retire 2cm de miolo formando uma rosca.</li>
-                    <li>Sove o restante da massa e abra novamente até cortar toda ela.</li>
-                    <li>Transfira os donuts para uma assadeira, cubra e deixe descansar por 1 hora ou até dobrar de tamanho.</li>
-                    <li>Frite, aos poucos, em óleo quente por 3 minutos ou até dourar.</li>
-                    <li>Escorra sobre papel-toalha e passe pelo açúcar misturando com canela.</li>
-                    <li>Por fim deixe esfriar e sirva em seguida.</li>
+            <div className="my-5 w-50 mx-auto fs-5">
+                <h2 className="text-danger">Preparo</h2>
+                <ol>
+                    {item.preparo}
                 </ol>
             </div>
 
