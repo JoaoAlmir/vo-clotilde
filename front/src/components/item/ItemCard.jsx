03b6bf2e6faa00acd40834610a2d71fb714712cf
JoaoAlmir <joaoalmir@alu.ufc.br>
2024-07-04 02:57:10 -0300

    add itemCard animation

--- front/src/components/item/ItemCard.jsx
+++ front/src/components/item/ItemCard.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
+import "./ItemCard.css";
 
 export function ItemCard({ detalhado, titulo, tempo, porcoes, dificuldade, imagem, link }) {
 
     return (
         (detalhado) ? (
-            <NavLink end to={"/detalhes/" + link} className="border-bottom border-5 rounded-5 shadow-lg border-danger-subtle me-5 label" style={{ width: "380px", height: "700px"}}>
+            <NavLink end to={"/detalhes/" + link} className="border-bottom border-5 rounded-5 shadow-lg border-danger-subtle me-5 item-card label" style={{ width: "380px", height: "700px"}}>
                 <img width="380px" height="250px" src={imagem} alt="icon" />
                 <h3 className="p-5 text-danger">  {titulo} </h3>
                 {dificuldade && <span className="d-flex flex-column ">
@@ -26,7 +27,7 @@ export function ItemCard({ detalhado, titulo, tempo, porcoes, dificuldade, image
             </NavLink>
         ) : (
             <NavLink end to={"/detalhes/" + link}>
-                <div className="border-bottom border-5 rounded-5 border-danger-subtle shadow-lg me-3 mb-3 label" style={{ width: "320px", height: "380px" }}>
+                <div className="border-bottom border-5 rounded-5 border-danger-subtle shadow-lg me-3 mb-3 label item-card" style={{ width: "320px", height: "380px" }}>
 
                     <img width="320px" height="200px" src={imagem} alt="icon" />
                     <h5 className="p-5 text-danger">{titulo}</h5>
