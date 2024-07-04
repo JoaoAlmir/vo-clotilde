67179cd304e4d1fc124001ab535aefa711d5cf6e
JoaoAlmir <joaoalmir@alu.ufc.br>
2024-07-04 01:53:56 -0300

    fix header optimization ref code

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,35 +1,36 @@
 import { NavLink } from "react-router-dom"
 import "./Header.css"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { AuthContext } from "../../App"
 
 function Header() {
-    let login = document.getElementsByName("login")
-    let conta = document.getElementsByName("conta")
-    let logout = document.getElementsByName("logout")
-    let create = document.getElementsByName("create")
+    const login = useRef(null);
+    const conta = useRef(null);
+    const logout = useRef(null);
+    const create = useRef(null);
 
     const auth = useContext(AuthContext);
     
 
     useEffect(() => {
 
-        if (auth.token) {
-            login[0].classList.add("d-none")
-            conta[0].classList.remove("d-none")
-            logout[0].classList.remove("d-none")
+        if (auth.token) {            
+            login.current.classList.add("d-none");
+            conta.current.classList.remove("d-none");
+            logout.current.classList.remove("d-none");
+            create.current.classList.remove("d-none");
         }
         else {
-            login[0].classList.remove("d-none")
-            conta[0].classList.add("d-none")
-            logout[0].classList.add("d-none")
+            login.current.classList.remove("d-none")
+            conta.current.classList.add("d-none")
+            logout.current.classList.add("d-none")
         }
 
         if(auth.funcao == 1){
-            create[0].classList.remove("d-none")
+            create.current.classList.remove("d-none")
         }
         else{
-            create[0].classList.add("d-none")
+            create.current.classList.add("d-none")
         }
     }, [auth.token, auth.funcao])
     
@@ -37,13 +38,13 @@ function Header() {
     return (
         <>
             <header className="d-flex justify-content-evenly position-sticky d-block shadow">
-                <NavLink end to="/login" name="login" className="header-button">
+                <NavLink end to="/login" ref={login} className="header-button">
                     <h5 className="p-4 text-danger header-buttom rounded-5 border-danger ">Login</h5>
                 </NavLink>
-                <div className="p-4" name="conta">
+                <div className="p-4" ref={conta}>
                     <h5>Olá {auth.nome}</h5>
                 </div>
-                <div className="p-4 text-danger header-buttom rounded-5 border-danger" id="logout" name="logout">
+                <div className="p-4 text-danger header-buttom rounded-5 border-danger" ref={logout}>
                     <h5 onClick={() => {
                         auth.setToken(null)
                         auth.setNome(null)
@@ -71,7 +72,7 @@ function Header() {
                 <NavLink end to="/categorias/pe">
                     <h5 className="text-danger header-buttom rounded-5 rounded-bottom border-danger border border-bottom-0">Peixes &amp; Frutos do Mar  </h5>
                 </NavLink>
-                <NavLink end to="/admin/add" name="create" className={"d-none"}>
+                <NavLink end to="/admin/add" ref={create} className={"d-none"}>
                     <h5 className="text-danger header-buttom rounded-5  border-danger border">+ </h5>
                 </NavLink>
 
