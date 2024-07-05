4b5f68aa13620ab3049305a19f832fe824640c3f
JoaoAlmir <joaoalmir@alu.ufc.br>
2024-07-05 02:52:01 -0300

    add createItem buttom

--- front/src/components/itemList/ItemList.jsx
+++ front/src/components/itemList/ItemList.jsx
@@ -1,24 +1,25 @@
 import { NavLink } from "react-router-dom";
 import "./ItemList.css";
 export function ItemList({ titulo, data, imagem, link }) {
-    titulo = "Capuccino de chocolate"
-    data = "15/15/2000"
-    imagem = "https://www.rbsdirect.com.br/imagesrc/23490969.jpg?version=1575255600"
-    link = "6686648ea9c80558b6712d5d"
     return (
         <>
-            <div className="d-flex rounded bg-danger-subtle">
+            <div className="d-flex rounded list-container justify-content-between">
+                <span>
                 <NavLink end to={"/detalhes/" + link} className="rounded" style={{ width: "120px", height: "80px" }}>
                     <img width="120px" height="85em" src={imagem} alt="icon" />
                 </NavLink>
-                <h4 className="p-4 ">  {titulo} </h4>
-                <h4 className="p-4"> {data} </h4>
-                <NavLink end to={"/edit/" + link} className="p-4 btn buttom-list border-black border-bottom">
-                    <i style={{ color: "blue"}}className="bi bi-pencil-square h3"></i>
-                </NavLink>
-                <button className="btn p-4 buttom-list border-black ">
-                    <i style={{ color: "red"}} className="bi bi-trash h3"></i>
-                </button>
+                <h4 className="p-4 d-inline">  {titulo} </h4>
+                </span>
+                <span>
+                    <h4 className="p-4 text-end d-inline"> {data} </h4>
+
+                    <NavLink end to={"/edit/" + link} className="p-4 btn buttom-list border-black border-bottom">
+                        <i style={{ color: "blue" }} className="bi bi-pencil-square h3"></i>
+                    </NavLink>
+                    <button className="btn p-4 buttom-list border-black ">
+                        <i style={{ color: "red" }} className="bi bi-trash h3"></i>
+                    </button>
+                </span>
             </div>
 
 
--- front/src/pages/admin/Admin.jsx
+++ front/src/pages/admin/Admin.jsx
@@ -1,13 +1,24 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import Header from "../../components/header/Header"
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../App";
 import { ItemList } from "../../components/itemList/ItemList";
+import { fetchItens } from "../../api/item";
 
 
 function Admin() {
     const nav = useNavigate();
     const auth = useContext(AuthContext);
+    const [itens, setItens] = useState([]);
+
+    useEffect(() => {
+        fetchItens().then((res) => {
+            setItens(res.data)
+        }
+        ).catch((err) => {
+            console.log(err)
+        })
+    }, [itens])
 
     // nav("/")
 
@@ -18,21 +29,40 @@ function Admin() {
             <Header />
 
 
-
             <div className="d-flex p-5 justify-content-center">
+
+
                 <div className="flex-column border-danger border rounded p-5">
-                    <ItemList />
-                    <ItemList />
-                    <ItemList />
+
+                    <div className="d-flex justify-content-center mb-2">
+                        <NavLink to="/admin/add" className="btn btn-danger rounded rounded-circle">
+                            <i  className="bi bi-plus-circle h1 p-3"></i>
+                        </NavLink>
+                    </div>
+
+                    {
+                        itens?.map((i) => (
+                            <div className="" key={i._id}>
+                                {i != undefined &&
+                                    <ItemList titulo={i.titulo} data={i.dataCriacao} imagem={i.imagem} link={i._id} />
+
+                                }
 
 
 
 
-                    
+                            </div>
+
+                        ))
+                    }
+
+
+
+
                 </div>
 
             </div >
-            
+
 
         </>
     )
