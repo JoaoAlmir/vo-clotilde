96a1aabb56a1bd8de5b227218c232479d7bb073a
JoaoAlmir <joaoalmir@alu.ufc.br>
2024-10-10 13:33:56 -0300

    change network PORT

--- back/main.js
+++ back/main.js
@@ -6,7 +6,7 @@ const db = require('./config/database.js');
 
 db('mongodb+srv://joaoalmircos:s7N6mseEFzIFbNqh@cluster0.dm00r.mongodb.net/voclotilde?retryWrites=true&w=majority&appName=Cluster0').then(_ => {
 
-    let port = process.env.PORT || 9999;
+    let port = process.env.PORT || 3000;
     let app = express_app(port);
 
     http.createServer(app).listen(app.get("port"), function () {
